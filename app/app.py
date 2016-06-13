e971e04f0ae8ec9e1c5a00601804e7701b9826d4
kelvinmuchui <muchuikelvin423@gmail.com>
2016-06-13 10:01:20 +0300

    post get request

--- app/app.py
+++ app/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-	return "thiss is on going"
+@app.route("/hello")
+def hello():
+	return "hello"
+
+#geting use post and adding
+@app.route("/", methods= ['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+    	value = int(request.form['number'])
+        add_one = value + 1
+        return render_template('index.html', string="TESTING", value = add_one)	
+    return render_template('index.html', string="TESTING")		    
 
 
 if __name__ == '__main__':
